b6396e4ac7283a1b489d6ca2f185ef765c464bd3
Pratik Kinage <pratikkinage@gmail.com>
2018-06-10 12:12:26 +0530

    Update rpicam.py

--- rpicam.py
+++ rpicam.py
@@ -6,9 +6,9 @@ import cv2
  
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
+camera.resolution = (320, 240)
+#camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(320, 240))
  
 # allow the camera to warmup
 time.sleep(0.1)
